42bb11a8b2c7fb3d54686ff5f080eb73f0db1aea
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-05-10 16:30:30 +0300

    test: add basic encodeToMorse error cases

--- test/encode-to-morse.test.ts
+++ test/encode-to-morse.test.ts
@@ -18,41 +18,35 @@ describe("encodeToMorse - Success cases", () => {
       expectedCode:
         ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..",
     },
-
     {
       name: "encodes A-Z letters",
       text: "ABCDEÉFGHIJKLMNOPQRSTUVWXYZ",
       expectedCode:
         ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..",
     },
-
     {
       name: "encodes 0-9 figures",
       text: "0123456789",
       expectedCode:
         "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.",
     },
-
     {
       name: "encodes punctuation marks and miscellaneous signs",
       text: `.,:?'"@=+-/()`,
       expectedCode:
         ".-.-.- --..-- ---... ..--.. .----. .-..-. .--.-. -...- .-.-. -....- -..-. -.--. -.--.-",
     },
-
     {
       name: "encodes a single letter",
       text: "a",
       expectedCode: ".-",
     },
-
     {
       name: "encodes mix of letters, figures and miscellaneous signs",
       text: "LoREm, 1p(SUM) 8o+L+oR: 3@it AMET",
       expectedCode:
         ".-.. --- .-. . -- --..-- / .---- .--. -.--. ... ..- -- -.--.- / ---.. --- .-.-. .-.. .-.-. --- .-. ---... / ...-- .--.-. .. - / .- -- . -",
     },
-
     {
       name: "ignores unknown letter when ignoreUnknown is true",
       text: "Nam vesti%bulum",
@@ -61,13 +55,11 @@ describe("encodeToMorse - Success cases", () => {
       },
       expectedCode: "-. .- -- / ...- . ... - .. -... ..- .-.. ..- --",
     },
-
     {
       name: "returns empty string for empty text",
       text: "",
       expectedCode: "",
     },
-
     {
       name: "uses custom chars from config",
       text: "Viverra pulvinar",
@@ -79,7 +71,6 @@ describe("encodeToMorse - Success cases", () => {
       },
       expectedCode: "aaa+_aa_aaa+_a_a+a_a+a_a+Ta++a_aa+_a+aa_aaa+_aa_+a_a+_a+a",
     },
-
     {
       name: "uses custom chars sequences from config",
       text: "Mauris eu erat",
@@ -92,13 +83,11 @@ describe("encodeToMorse - Success cases", () => {
       expectedCode:
         ".b.b.- / a-.b.- / a-a-.b.- / a-.ba-.- / a-a-.- / a-a-a- ca-.- / a-a-.b ca-.- / a-.ba-.- / a-.b.- / .b",
     },
-
     {
       name: "handles leading, trailing, and multiple consecutive spaces",
       text: " Aliquam   erat  ",
       expectedCode: ".- .-.. .. --.- ..- .- -- / . .-. .- -",
     },
-
     {
       name: "returns empty string when text contains only unknown chars and ignoreUnknown is true",
       text: "#$%&",
@@ -107,7 +96,6 @@ describe("encodeToMorse - Success cases", () => {
       },
       expectedCode: "",
     },
-
     {
       name: "returns empty string when text contains only various whitespaces (tabs, newlines)",
       text: "   \n \t  ",
@@ -116,7 +104,6 @@ describe("encodeToMorse - Success cases", () => {
       },
       expectedCode: "",
     },
-
     {
       name: "handles empty strings as custom chars",
       text: "Donec arcu",
@@ -128,7 +115,6 @@ describe("encodeToMorse - Success cases", () => {
       },
       expectedCode: "",
     },
-
     {
       name: "ignores unknown properties in the options object",
       text: "Tempor quis",
@@ -148,7 +134,82 @@ describe("encodeToMorse - Success cases", () => {
 });
 
 describe("encodeToMorse - Error cases", () => {
-  const testCases: TestCase[] = [];
+  const testCases: TestCase[] = [
+    {
+      name: "throws on unknown letter when ignoreUnknown is false",
+      text: "Vivamus portt%itor",
+      options: {
+        ignoreUnknown: false,
+      },
+      expectedError: "Invalid input: letter % is not allowed in text",
+    },
+    {
+      name: "throws when text is not a string",
+      text: 15 as any,
+      expectedError: "Invalid input: expected text to be a string",
+    },
+    {
+      name: "throws when options is a number",
+      text: "Aenean facilisis",
+      options: 15,
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options is a null",
+      text: "Fusce semper",
+      options: null,
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options is an array",
+      text: "Aliquam eget",
+      options: ["array"],
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options.dot is not a string",
+      text: "Aenean ac",
+      options: {
+        dot: 15,
+      } as any,
+      expectedError: "Invalid input: expected options.dot to be a string",
+    },
+    {
+      name: "throws when options.dash is not a string",
+      text: "Donec tristique",
+      options: {
+        dash: true,
+      } as any,
+      expectedError: "Invalid input: expected options.dash to be a string",
+    },
+    {
+      name: "throws when options.tokenSeparator is not a string",
+      text: "Integer vel",
+      options: {
+        tokenSeparator: ["not", "a", "string"],
+      } as any,
+      expectedError:
+        "Invalid input: expected options.tokenSeparator to be a string",
+    },
+    {
+      name: "throws when options.symbolSeparator is not a string",
+      text: "Suspendisse malesuada",
+      options: {
+        symbolSeparator: { not: "a string" },
+      } as any,
+      expectedError:
+        "Invalid input: expected options.symbolSeparator to be a string",
+    },
+    {
+      name: "throws when options.ignoreUnknown is not a boolean",
+      text: "Aenean et",
+      options: {
+        ignoreUnknown: null,
+      } as any,
+      expectedError:
+        "Invalid input: expected options.ignoreUnknown to be a boolean",
+    },
+  ];
 
   it.for(testCases)("$name", ({ text, options, expectedError }) => {
     expect(() => encodeToMorse(text, options)).toThrow(expectedError);
