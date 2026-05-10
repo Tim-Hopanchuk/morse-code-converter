9c9fc19439c670c409910f084a1b1925e879a635
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-05-10 16:31:03 +0300

    test: add basic decodeFromMorse error cases

--- test/decode-from-morse.test.ts
+++ test/decode-from-morse.test.ts
@@ -17,31 +17,26 @@ describe("decodeFromMorse - Success cases", () => {
       code: ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..",
       expectedText: "ABCDEÉFGHIJKLMNOPQRSTUVWXYZ",
     },
-
     {
       name: "decodes 0-9 figures",
       code: "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.",
       expectedText: "0123456789",
     },
-
     {
       name: "decodes punctuation marks and miscellaneous signs",
       code: ".-.-.- --..-- ---... ..--.. .----. .-..-. .--.-. -...- .-.-. -....- -..-. -.--. -.--.-",
       expectedText: `.,:?'"@=+-/()`,
     },
-
     {
       name: "decodes a single letter",
       code: ".-",
       expectedText: "A",
     },
-
     {
       name: "decodes mix of letters, figures and miscellaneous signs",
       code: ".-.. --- .-. . -- --..-- / .---- .--. -.--. ... ..- -- -.--.- / ---.. --- .-.-. .-.. .-.-. --- .-. ---... / ...-- .--.-. .. - / .- -- . -",
       expectedText: "LOREM, 1P(SUM) 8O+L+OR: 3@IT AMET",
     },
-
     {
       name: "ignores unknown symbol when ignoreUnknown is true",
       code: "-. .- -- / ...- % . ... - .. -... ..- .-.. ..- --",
@@ -50,13 +45,11 @@ describe("decodeFromMorse - Success cases", () => {
       },
       expectedText: "NAM VESTIBULUM",
     },
-
     {
       name: "returns empty string for empty code",
       code: "",
       expectedText: "",
     },
-
     {
       name: "uses custom chars from config",
       code: "aaa+_aa_aaa+_a_a+a_a+a_a+Ta++a_aa+_a+aa_aaa+_aa_+a_a+_a+a",
@@ -68,7 +61,6 @@ describe("decodeFromMorse - Success cases", () => {
       },
       expectedText: "VIVERRA PULVINAR",
     },
-
     {
       name: "uses custom chars sequences from config",
       code: ".b.b.- / a-.b.- / a-a-.b.- / a-.ba-.- / a-a-.- / a-a-a- ca-.- / a-a-.b ca-.- / a-.ba-.- / a-.b.- / .b",
@@ -80,16 +72,14 @@ describe("decodeFromMorse - Success cases", () => {
       },
       expectedText: "MAURIS EU ERAT",
     },
-
     {
       name: "returns empty string when code contains only unknown chars and ignoreUnknown is true",
       code: "#$%&",
       options: {
-        ignoreUnknown: false,
+        ignoreUnknown: true,
       },
       expectedText: "",
     },
-
     {
       name: "ignores unknown properties in the options object",
       code: "- a -- a--a --- a-a / --a- aa- aa aaa",
@@ -109,7 +99,82 @@ describe("decodeFromMorse - Success cases", () => {
 });
 
 describe("decodeFromMorse - Error cases", () => {
-  const testCases: TestCase[] = [];
+  const testCases: TestCase[] = [
+    {
+      name: "throws on unknown symbol when ignoreUnknown is false",
+      code: "-.. --- -. . -.-. / .%- / -.. --- .-.. --- .-.",
+      options: {
+        ignoreUnknown: false,
+      },
+      expectedError: "Invalid input: symbol .%- is not allowed in code",
+    },
+    {
+      name: "throws when code is not a string",
+      code: false as any,
+      expectedError: "Invalid input: expected code to be a string",
+    },
+    {
+      name: "throws when options is a string",
+      code: ".- . -. . .- -. / ..-. .- -.-. .. .-.. .. ... .. ...",
+      options: "not an object",
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options is a null",
+      code: "..-. ..- ... -.-. . / ... . -- .--. . .-.",
+      options: null,
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options is an array",
+      code: ".- .-.. .. --.- ..- .- -- / . --. . -",
+      options: ["not", "an", "object"],
+      expectedError: "Invalid input: expected options to be an object",
+    } as any,
+    {
+      name: "throws when options.dot is not a string",
+      code: ".- . -. . .- -. / .- -.-.",
+      options: {
+        dot: NaN,
+      } as any,
+      expectedError: "Invalid input: expected options.dot to be a string",
+    },
+    {
+      name: "throws when options.dash is not a string",
+      code: "-.. --- -. . -.-. / - .-. .. ... - .. --.- ..- .",
+      options: {
+        dash: true,
+      } as any,
+      expectedError: "Invalid input: expected options.dash to be a string",
+    },
+    {
+      name: "throws when options.tokenSeparator is not a string",
+      code: ".. -. - . --. . .-. / ...- . .-..",
+      options: {
+        tokenSeparator: 95,
+      } as any,
+      expectedError:
+        "Invalid input: expected options.tokenSeparator to be a string",
+    },
+    {
+      name: "throws when options.symbolSeparator is not a string",
+      code: ".- . -. . .- -. / . -",
+      options: {
+        symbolSeparator: ["array"],
+      } as any,
+      expectedError:
+        "Invalid input: expected options.symbolSeparator to be a string",
+    },
+    {
+      name: "throws when options.ignoreUnknown is not a boolean",
+      code: "... ..- ... .--. . -. -.. .. ... ... . / -- .- .-.. . ... ..- .- -.. .-",
+      options: {
+        ignoreUnknown: 0,
+      } as any,
+      expectedError:
+        "Invalid input: expected options.ignoreUnknown to be a boolean",
+    },
+  ];
 
   it.for(testCases)("$name", ({ code, options, expectedError }) => {
     expect(() => decodeFromMorse(code, options)).toThrow(expectedError);
